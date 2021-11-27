7ac6bbccc17704e4445c070489243130d6454328
Luis Guanes <luis@toky.co>
2021-11-26 21:28:35 -0300

    added translations + bootstap elements + flags lib + ltr text on document

--- src/index.js
+++ src/index.js
@@ -1,44 +1,42 @@
-import React from 'react';
+import React, { Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'flag-icon-css/css/flag-icons.min.css';
 
-import './index.css';
-// import App from './App';
+import App from './App';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    
+    supportedLngs: ["en", "fr", "ar"],    
     fallbackLng: "en",
     detection: {
-      order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+      order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
       caches: ['cookie']
     },
     backend: {
       loadPath: '/assets/locales/{{lng}}/translation.json'
-    },
-    react: {useSuspense: false}
+    }
   });
 
-function App() {
-  const { t } = useTranslation();
-  return <h2>{t('welcome_to_react')}</h2>;
-}
-
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h2>Loading..</h2>  
+  </div>
+  )
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
   document.getElementById('root')
 );
